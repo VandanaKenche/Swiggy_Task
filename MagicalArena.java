4617691a59fa6b41e4836f9e270b9141f41c9f59
Vandana <vandana.kenche123@gmail.com>
2024-06-23 20:37:33 +0530

    5th commit

--- MagicalArena.java
+++ MagicalArena.java
@@ -41,4 +41,51 @@ public class MagicalArena {
         }
     }
 
+    public static class Arena {
+        private Player player1;
+        private Player player2;
+
+        public Arena(Player player1, Player player2) {
+            this.player1 = player1;
+            this.player2 = player2;
+        }
+
+        public void startFight() {
+            Player attacker, defender;
+
+            if (player1.getHealth() < player2.getHealth()) {
+                attacker = player1;
+                defender = player2;
+            } else {
+                attacker = player2;
+                defender = player1;
+            }
+
+            while (player1.isAlive() && player2.isAlive()) {
+                System.out.println("Attacker: " + (attacker == player1 ? "Player 1" : "Player 2"));
+                int attackDice = attacker.rollDice();
+                int defendDice = defender.rollDice();
+
+                int damage = attacker.getAttack() * attackDice - defender.getStrength() * defendDice;
+                damage = Math.max(damage, 0);
+
+                defender.takeDamage(damage);
+                System.out.println("Attacker rolls: " + attackDice + " | Defender rolls: " + defendDice);
+                System.out.println("Damage dealt: " + damage);
+                System.out.println("Defender's health: " + defender.getHealth());
+
+                Player temp = attacker;
+                attacker = defender;
+                defender = temp;
+            }
+
+            if (player1.isAlive()) {
+                System.out.println("Player 1 wins!");
+            } else {
+                System.out.println("Player 2 wins!");
+            }
+        }
+    }
+
+
     }
